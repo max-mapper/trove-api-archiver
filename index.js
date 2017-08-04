7c939e9a1addc2e4f7f4b6ee5cce604810eca709
Max Ogden <max@maxogden.com>
2017-08-04 14:05:16 -0700

    first version

---
+++ index.js
@@ -0,0 +1,59 @@
+var request = require('request')
+var fs = require('fs')
+
+var zones = ["book", "picture", "article", "music", "map", "collection", "newspaper", "list"]
+var key = '&key=' + process.argv[2]
+var since = process.argv[3] || 0
+var oneZone = process.argv[4] || false
+var root = 'http://api.trove.nla.gov.au'
+if (oneZone) zones = [oneZone]
+
+var urls = zones.map(function (z) {
+  return {zone: z, url: `${root}/result?q=lastupdated:[*%20TO%20*]&zone=${z}&encoding=json${key}&s=${since}&n=100&sortby=dateasc`}
+})
+
+function getNext() {
+  var next = urls.shift()
+  if (!next) return
+  get(next, getNext)
+}
+
+get(urls[0], function () {
+  console.log('done')
+})
+
+function get (item, cb) {
+  var out = fs.createWriteStream('./' + item.zone + '-zone.json')
+  req(item.url)
+  var retries = 3
+  function req (url) {
+    var start = Date.now()
+    request(url, {json: true}, function (err, resp, data) {
+      if (err || resp.statusCode > 299) {
+        if (retries > 0) {
+          retries--
+          console.error('retry', retries, url, resp.statusCode, data)
+          setTimeout(15000, req(url))
+          return
+        }
+        throw err
+      }
+      var records = data.response.zone[0].records
+      var next = records.next
+      if (!next || !records.work.length) {
+        out.end()
+        return cb()
+      }
+      console.log(start, records.work.length, url)
+      records.work.forEach(function (r) {
+        out.write(JSON.stringify(r) + '\n')
+      })
+      var since = Date.now() - start
+      if (since < 3600) setTimeout(doNext, 3600 - since) // keep under 1000/hr rate limit
+      else doNext()
+      function doNext () {
+        req(root + next + key)
+      }
+    })
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# trove-api-archiver
+
+download metadata from the national library of australia NLA trove api
+
+```sh
+$ npm i trove-api-archiver -g
+$ trove-api-archiver <troveApiKey>
+# it will download all the metadata as json files in the current dir
+```
